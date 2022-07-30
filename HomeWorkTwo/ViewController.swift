6f92d0790d18b9f177b87b623ef2ba71d95348df
teppIu <satsteo@gmail.com>
2022-07-30 19:22:57 +0500

    Изменение цвета вью при нажатии на кнопку.

--- HomeWorkTwo/ViewController.swift
+++ HomeWorkTwo/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        redView.backgroundColor = .red
+        redView.alpha = 0.5
+        redView.layer.cornerRadius = 75
+        
+        yellowView.backgroundColor = .yellow
+        yellowView.alpha = 0.5
+        yellowView.layer.cornerRadius = 75
+        
+        greenView.backgroundColor = .green
+        greenView.alpha = 0.5
+        greenView.layer.cornerRadius = 75
+        
+        nextButton.tintColor = .black
+        nextButton.backgroundColor = .white
+        nextButton.layer.cornerRadius = 17
+        nextButton.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+        nextButton.setTitle("NEXT", for: .normal)
+        
     }
 
-
+    @IBAction func changeColorBotton() {
+        if redView.alpha == 0.5 {
+            redView.alpha = 1
+        } else if redView.alpha == 1 {
+            redView.alpha = 0.4
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.4
+            greenView.alpha = 1
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.4
+            redView.alpha = 1
+        }
+    }
+        
+    
+    
 }
 
